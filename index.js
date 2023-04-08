b762e2b1b50aefdff21fa3494140960a8af2753d
Cliffton Kaewkarn <ck@Clifftons-MacBook-Air.local>
2023-04-08 16:14:30 +0700

    Add embedded expressions and other strings into the react dom render. Not sure what im doing but results are showing accordingly in the browser.

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const header =
+  <h1>Arigato React</h1>;
+var name = "React Smith";
+var url = "https://mimo.app/i/lori.png";
+
+var embeddedExpression = 
+<p>This is embedded expression</p>;
+
+const app = (
+  <div>
+    <img src={url} alt="loris profile"></img>
+    </div>
+);
+
 ReactDOM.render(
-<h1>Hello React</h1>,
-document.getElementById("root")
+<div>
+<h1>Embedded Expression</h1>
+<h2>See below</h2>
+{embeddedExpression}
+</div>,
+  document.getElementById("root")
 );
